4f989d8f5703ac07f1c24748b0723ed47776f729
Euler Pedraza <epedraza@norwu.com>
2023-08-11 20:28:04 -0600

    Minimal functional exmaple of New Relic custom instrumentation
    We use the method startBacgroundTransaction to create a custom non-web transaction
    We use the method startSegment to get a deeper visibility of the transaction

--- simple/src/index.js
+++ simple/src/index.js
@@ -1,9 +1,40 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Track the number of requests
+var total = 1;
+
+
+// Instrument an custom non web transaction
+const transaction = newrelic.startBackgroundTransaction('customTransaction', customTransaction);
+
+function customTransaction() {
+  total = total + 1;
+  // Gain more visibility into transaction by using startSegment
+  const result = newrelic.startSegment('factorial',false, () => { return factorial(total); });
+  return result;
+};
+
+
+// Recursive version of factorial function
+function factorial(x) {
+
+    if (x == 0) {
+        return 1;
+    }
+
+    else {
+        return x * factorial(x - 1);
+    }
+}
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const factorial = customTransaction();
+    res.send('Hello World!' + '\nTotal:' + factorial);
 });
 
 app.listen(port, () => {
